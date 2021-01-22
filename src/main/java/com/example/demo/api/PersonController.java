3e7f51899c9656ba7dd4cb4260c741b274550458
Kartik <kartik@hyperverge.co>
2021-01-22 23:48:37 +0530

    Add select person by id route

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,10 +3,11 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -20,5 +21,13 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+    @GetMapping(path = "/{id}")
+    public Optional<Person> getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id);
     }
  }
